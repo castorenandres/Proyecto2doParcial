d57f95b38e6c3d4de72bbad1d35ed00521fae742
Fernando Leal <lealp.fernando@gmail.com>
2019-10-23 22:48:38 -0500

    Background.ts y agregado playing.jpg

---
+++ src/Background.ts
@@ -0,0 +1,25 @@
+import Playing from "./Scences/Playing"
+import background from "./assets/Playing.jpg"
+import GameContext from "./GameContext";
+class Background{
+    private backgroundImage = new Image ();
+    private playingScene: Playing = null;
+    
+    constructor(playingScene: Playing){
+        this.playingScene = playingScene; 
+    }
+    
+    public render = () =>{
+        this.backgroundImage.src = background;
+
+        const {context} = GameContext;
+        const height = context.canvas.height;
+        const naturalHeight = this.backgroundImage.naturalHeight;
+        const naturalWidth = this.backgroundImage.naturalWidth;
+        
+        const finalImageWidth = ((naturalWidth*height)/naturalHeight);
+        const finalImageHeight = height;
+
+        context.drawImage(this.backgroundImage, finalImageWidth, 0, finalImageWidth, finalImageHeight);
+    }
+}
