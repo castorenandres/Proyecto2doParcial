3c41b6ba70ef4b4186a3dc387e8eef3542c14e49
FernandoL <lealp.fernando@gmail.com>
2019-10-23 20:11:33 -0500

    Agregar engine y algunos events listeners

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,11 @@
+import GameEngine from "./Engine"
 const canvas = document.getElementById("game-area") as HTMLCanvasElement;
 const context = canvas.getContext("2d");
+
+const engine = new GameEngine();
+engine.start();
+
+canvas.addEventListener("mousedown", engine.mouseDownListener);
+canvas.addEventListener("mousemove", engine.mouseDownListener);
+canvas.addEventListener("mouseout", engine.mouseDownListener);
+canvas.addEventListener("mousemoup", engine.mouseDownListener);
