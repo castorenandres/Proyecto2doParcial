cf0c04a69d5fbc177de64612693079ab1540b689
FernandoL <lealp.fernando@gmail.com>
2019-10-23 20:47:41 -0500

    Comentado inedx.ts

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,11 @@ const canvas = document.getElementById("game-area") as HTMLCanvasElement;
 const context = canvas.getContext("2d");
 
 const engine = new GameEngine();
+/*
 engine.start();
 
 canvas.addEventListener("mousedown", engine.mouseDownListener);
 canvas.addEventListener("mousemove", engine.mouseDownListener);
 canvas.addEventListener("mouseout", engine.mouseDownListener);
 canvas.addEventListener("mousemoup", engine.mouseDownListener);
+*/
